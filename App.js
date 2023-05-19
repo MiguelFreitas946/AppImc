7122c1e7c6836547ce0c8ce53923e3b644abc1da
miguel freitas <miguelfreitas946@gmail.com>
2023-05-19 22:58:18 +0000

    imc dahora

--- App.js
+++ App.js
@@ -1,11 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Component} from 'react-native';
 
 export default function App() {
+
+	const[peso, setPeso] = useState();
+	const[altura, setAltura] = useState();
+	const[resultado, setResultado] = useState();
+	const[nivel, setNivel] = useState();
+
+	
+	function imc(){
+		let r = peso / (altura * altura);
+		setResultado(r.toFixed(2));
+		
+		if (r <= 16.9){
+			let n = ("Muito abaixo do peso!");
+			setNivel(n);		
+		}
+		
+		else if (r >= 17 && r <= 18.4){
+			let n = ("Abaixo do peso!");
+			setNivel(n);
+		}
+		
+		else if (r >= 18.5 && r <= 24.9){
+			let n = ("Peso normal!");
+			setNivel(n);
+		}
+		
+		else if (r >= 25 && r <= 29.9){
+			let n = ("Acima do peso!");
+			setNivel(n);
+		}
+		
+		else if (r >= 30 && r <= 34.9){
+			let n = ("Obesidade grau I!");
+			setNivel(n);
+		}
+		
+		else if (r >= 35 && r <= 40){
+			let n = ("Obesidade grau II!");
+			setNivel(n);
+		}
+		
+		else if (r >= 40){
+			let n = ("Obesidade grau III!");
+			setNivel(n);
+		}
+	}
+	
+	function limpar(){
+		setResultado("");
+		setAltura("");
+		setPeso("");
+	}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+		  <View style={styles.titulo}>
+			  <Text style={styles.textoTitulo}>CALCULADORA IMC</Text>
+		  </View>
+		  
+		  <View style={styles.blocoImagem}>
+		  		<Image
+				 	style={styles.imagem}
+				 	source={require('./assets/gordo.jpg')}
+				/>
+		  </View>
+		  
+		  
+		  <View style={styles.bloco}>
+				<Text style={styles.label}>Peso (KG): </Text>
+			  	<TextInput
+					maxLength={6}
+					style={styles.input}
+					value={peso}
+					onChangeText={(valor)=>setPeso(valor)}
+					placeholder="Exemplo 64.350"
+					placeholderTextColor="#b1aeae"
+				/>
+			  
+		  		<Text style={styles.label}>Altura (Metro): </Text>
+			  	<TextInput
+					maxLength={6}
+					style={styles.input}
+					value={altura}
+					onChangeText={(valor)=>setAltura(valor)}
+					placeholder="Exemplo 1.72"
+					placeholderTextColor="#b1aeae" 
+				/>
+			  
+			  <View style={styles.blocoBotao}>
+				<TouchableOpacity style={styles.botao} onPress={imc}>
+					<Text style={styles.textoBotao}>Calcular</Text>
+				</TouchableOpacity>
+				  
+				<TouchableOpacity style={styles.botao} onPress={limpar}>
+					<Text style={styles.textoBotao}>Limpar</Text>
+				</TouchableOpacity>
+			  </View>
+			  
+			<View style={styles.label1}>
+				<Text style={styles.label1}> Resultado: {resultado} </Text>
+			</View>
+			
+			<View style={styles.label1}>
+				<Text style={styles.label1}> Classificação: {nivel} </Text>
+			</View>
+			
+		  </View>
+      
     </View>
   );
 }
@@ -13,8 +117,73 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	backgroundColor: '#fff'
   },
+	
+	titulo: {
+		backgroundColor: '#f0f0f0',
+		height: '10%',
+		justifyContent: 'center'
+	},
+	
+	textoTitulo: {
+		fontSize: 20,
+		textAlign: 'center',
+		color: '#fff'
+	},
+				  
+	imagem: {
+		width: 120,
+		height: 120
+	},
+	
+	blocoImagem: {
+		alignItems: 'center',
+		marginTop: 30
+	},
+	
+	bloco:{
+		width: '70%',
+		marginLeft: '15%'
+		
+	},
+
+	input: {
+	 	backgroundColor: '#fff',
+		borderWidth: 1,
+		fontSize: 15,
+		borderRadius: 5
+	},
+	
+	label: {
+		fontSize: 15,
+		marginTop: 10
+	},
+	
+	label1: {
+		fontSize: 15,
+		marginTop: 10,
+		textAlign: 'center'
+	},
+	
+	
+	blocoBotao:{
+		flexDirection: 'row',
+		justifyContent: 'center'
+	},
+	
+	botao:{
+		width: '25%',
+		marginTop: 20,
+		marginLeft: '05%',
+		marginRight: '05%',
+		alignItems: 'center',
+		backgroundColor: '#f0f0f0',
+		borderWidth: 1
+	},
+	
+	textoBotao: {
+		fontSize: 15,
+		textAlign: 'center'		
+	},
 });
